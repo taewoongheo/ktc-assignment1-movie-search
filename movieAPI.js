00baf8b49fe08f23b38d2f6d423d19f24959ec69
taewoongheo <chamjoeun0111@gmail.com>
2025-04-29 19:21:48 +0900

    feat: TMDB에서 영화데이터를 가져오는 기능 추가

---
+++ movieAPI.js
@@ -0,0 +1,19 @@
+import { API_KEY } from "./auth.js";
+
+function getMovieInfo() {
+  const url = `https://api.themoviedb.org/3/movie/now_playing?&language=ko-KR&region=KR&api_key=${API_KEY}`;
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.error(err));
+}
+
+getMovieInfo();
