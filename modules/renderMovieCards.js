858b0176056a7fdf3826921c24e0cb1734effc82
taewoongheo <chamjoeun0111@gmail.com>
2025-05-01 21:27:31 +0900

    feat: 영화카드 클릭 시 상세정보 모달을 띄우는 기능 구현

--- modules/renderMovieCards.js
+++ modules/renderMovieCards.js
@@ -1,3 +1,4 @@
+import { API_KEY } from "../auth.js";
 import { getMovieData } from "./movieAPI.js";
 
 export async function renderMovieCards() {
@@ -6,9 +7,9 @@ export async function renderMovieCards() {
   try {
     const data = await getMovieData();
     for (const movie of data.results) {
-      const { poster_path, title, overview, vote_average } = movie;
+      const { id, poster_path, title, overview, vote_average } = movie;
       $movieListSection.appendChild(
-        createMovieCardElement(poster_path, title, overview, vote_average)
+        createMovieCardElement(poster_path, title, overview, vote_average, id)
       );
     }
   } catch (err) {
@@ -16,7 +17,7 @@ export async function renderMovieCards() {
   }
 }
 
-function createMovieCardElement(img, title, desc, rating) {
+function createMovieCardElement(img, title, desc, rating, id) {
   const card = document.createElement("article");
   card.classList.add("movie-card");
 
@@ -39,5 +40,75 @@ function createMovieCardElement(img, title, desc, rating) {
   ratingDiv.append(ratingElem);
 
   card.append(posterElem, titleElem, descElem, ratingDiv);
+
+  card.addEventListener("click", async () => {
+    const modalContainer = document.querySelector(".modalContainer");
+    const modal = document.querySelector(".modal");
+
+    modalContainer.classList.remove("hidden");
+    modalContainer.addEventListener("click", (e) => {
+      if (e.target !== e.currentTarget) return;
+
+      modalContainer.classList.add("hidden");
+      modal.innerHTML = "";
+    });
+
+    const url = `https://api.themoviedb.org/3/movie/${id}?&language=ko-KR&region=KR&api_key=${API_KEY}`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
+    };
+
+    try {
+      const data = await fetch(url, options);
+      const json = await data.json();
+
+      const {
+        title,
+        genres,
+        overview,
+        poster_path,
+        release_date,
+        runtime,
+        vote_average,
+      } = json;
+
+      const modalTitleElem = document.createElement("h1");
+      modalTitleElem.textContent = title;
+
+      const modalGenresElem = document.createElement("p");
+      modalGenresElem.textContent = genres.map((el) => el.name).join(", ");
+
+      const modalOverviewElem = document.createElement("p");
+      modalOverviewElem.textContent = overview;
+
+      const modalPosterElem = document.createElement("img");
+      modalPosterElem.src = `https://image.tmdb.org/t/p/w154${poster_path}`;
+
+      const modalReleaseElem = document.createElement("p");
+      modalReleaseElem.textContent = release_date;
+
+      const modalRuntimeElem = document.createElement("p");
+      modalRuntimeElem.textContent = runtime;
+
+      const modalRatingElem = document.createElement("p");
+      modalRatingElem.textContent = vote_average;
+
+      modal.append(
+        modalTitleElem,
+        modalGenresElem,
+        modalOverviewElem,
+        modalPosterElem,
+        modalReleaseElem,
+        modalRuntimeElem,
+        modalRatingElem
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
   return card;
 }
